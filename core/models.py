edfc81c38cfbab5c994b199d3ccae3d48c3fe055
Joselio Bezera <joseliobeze@gmail.com>
2019-10-26 00:33:15 -0300

    criado filtros

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -10,7 +11,16 @@ class Evento(models.Model):
     descricao = models.TextField(
         blank=True, null=True
         )
-    data_evento = models.DateTimeField()
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(
         auto_now=True
         )
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE
+        )
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
