231602f20d3180d0ad0b2b58f0d07900617a35e7
Joselio Bezera <joseliobeze@gmail.com>
2019-10-26 23:51:41 -0300

    adicionando campo local na na tabela evento

--- core/models.py
+++ core/models.py
@@ -11,6 +11,7 @@ class Evento(models.Model):
     descricao = models.TextField(
         blank=True, null=True
         )
+    local = models.CharField(max_length=100, blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(
         auto_now=True
