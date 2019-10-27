c5fb61abf0c288402d51827a5e2b6d9700a4389e
Joselio Bezera <joseliobeze@gmail.com>
2019-10-26 22:15:01 -0300

    ciado pagina de login

--- core/views.py
+++ core/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Evento
 
-
 # Create your views here.
-def lista_eventos(request):
+def login_user(request):
+    return render(request, 'login.html')
 
-    evento = Evento.objects.all()
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password') 
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou Senha invalido")
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
