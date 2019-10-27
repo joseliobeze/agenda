ce2c9b606401b734f7f740c1c04e34a4fc3f0777
Joselio Bezera <joseliobeze@gmail.com>
2019-10-26 23:37:05 -0300

    incluido o tempplate para inserçaode arquivos

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Evento
 
+
 # Create your views here.
 def login_user(request):
     return render(request, 'login.html')
@@ -17,7 +18,7 @@ def logout_user(request):
 def submit_login(request):
     if request.POST:
         username = request.POST.get('username')
-        password = request.POST.get('password') 
+        password = request.POST.get('password')
         usuario = authenticate(username=username, password=password)
         if usuario is not None:
             login(request, usuario)
@@ -33,3 +34,24 @@ def lista_eventos(request):
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(
+            titulo=titulo,
+            data_evento=data_evento,
+            descricao=descricao,
+            usuario=usuario
+            )
+    return redirect('/')
